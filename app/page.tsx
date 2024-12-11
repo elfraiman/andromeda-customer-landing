cf2e3d2587fc11b945bf04d69efcfcfd86557088
Elan Fraiman <elfraiman@gmail.com>
2024-12-11 12:58:48 +0200

    chore(cleanup): cleanup imports and redundent code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import LandingPage from './landing/page';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
-const theme = createTheme();
 
 export default function Home() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <ThemeProvider theme={theme}>
-        <LandingPage />
-      </ThemeProvider>
-    </LocalizationProvider >
+    <LandingPage />
   )
 }
